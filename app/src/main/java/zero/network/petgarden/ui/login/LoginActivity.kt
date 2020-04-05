6cf85166d5f9f6ae179907fd332570bbaf930bd9
CarlosEduardoL <carlos.lizalda1@hotmail.com>
2020-04-04 23:25:10 -0500

    vista del fragmento registro nacimiento completada

--- app/src/main/java/zero/network/petgarden/ui/login/LoginActivity.kt
+++ app/src/main/java/zero/network/petgarden/ui/login/LoginActivity.kt
@@ -13,6 +13,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
 import zero.network.petgarden.R
+import zero.network.petgarden.ui.register.RegisterActivity
 
 
 class LoginActivity : AppCompatActivity() {
@@ -38,8 +39,10 @@ class LoginActivity : AppCompatActivity() {
             setSpan(
                 object : ClickableSpan() {
                     override fun onClick(widget: View) {
-                        println("Pase")
-                        showToast("Nada por ahora")
+                        Intent(this@LoginActivity, RegisterActivity::class.java).apply {
+                            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                            startActivity(this)
+                        }
                     }
                 },
                 content.indexOf(lastWord),
--- app/src/main/java/zero/network/petgarden/ui/register/RegisterActivity.kt
+++ app/src/main/java/zero/network/petgarden/ui/register/RegisterActivity.kt
@@ -3,11 +3,26 @@ package zero.network.petgarden.ui.register
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import zero.network.petgarden.R
+import zero.network.petgarden.RegisterBirthFragment
+import zero.network.petgarden.ui.element.ActionBarFragment
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var nameFragment: RegisterNameFragment
+    private lateinit var emailFragment: RegisterEmailFragment
+    private lateinit var birthFragment: RegisterBirthFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        supportFragmentManager.beginTransaction().apply{
+            replace(R.id.header, ActionBarFragment("Register", true){finish()})
+            commit()
+        }
+        birthFragment = RegisterBirthFragment()
+        supportFragmentManager.beginTransaction().apply{
+            replace(R.id.body, birthFragment)
+            commit()
+        }
     }
 }
