c1eedd4311ed863554f8b5122569be66a7e37cf5
CarlosEduardoL <carlos.lizalda1@hotmail.com>
2020-04-05 12:52:14 -0500

    Vista registro nombre mascota terminada

---
+++ app/src/main/java/zero/network/petgarden/ui/register/PetNameRegisterFragment.kt
@@ -0,0 +1,23 @@
+package zero.network.petgarden.ui.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import zero.network.petgarden.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class PetNameRegisterFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_pet_name_register, container, false).apply {
+
+    }
+
+}
