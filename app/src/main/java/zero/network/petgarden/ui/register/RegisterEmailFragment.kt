a1792ef0c70f9ba64b8f626297799c0927795b22
CarlosEduardoL <carlos.lizalda1@hotmail.com>
2020-04-04 22:49:53 -0500

    Vista del fragmento registro correo electronico terminada

---
+++ app/src/main/java/zero/network/petgarden/ui/register/RegisterEmailFragment.kt
@@ -0,0 +1,21 @@
+package zero.network.petgarden.ui.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import zero.network.petgarden.R
+
+
+class RegisterEmailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_register_email, container, false).apply{
+
+    }
+
+}
