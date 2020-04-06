54d96e21e3f792bb019e3619d466e50061f1bb73
CarlosEduardoL <carlos.lizalda1@hotmail.com>
2020-04-06 12:12:51 -0500

    vista del fragmento registro de contraseña terminada

---
+++ app/src/main/java/zero/network/petgarden/ui/register/PasswordRegisterFragment.kt
@@ -0,0 +1,24 @@
+package zero.network.petgarden.ui.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import zero.network.petgarden.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class PasswordRegisterFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_password_register, container, false)
+    }
+
+}
