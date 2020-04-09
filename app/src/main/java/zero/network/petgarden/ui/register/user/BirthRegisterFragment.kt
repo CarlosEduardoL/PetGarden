bf7ae1f74e108706fee3b955a6e66c1348ec5c77
CarlosEduardoL <carlos.lizalda1@hotmail.com>
2020-04-09 10:10:11 -0500

    Logica del fragmento registro decumpleaños completada

--- app/src/main/java/zero/network/petgarden/ui/register/user/BirthRegisterFragment.kt
+++ app/src/main/java/zero/network/petgarden/ui/register/user/BirthRegisterFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_register_birth.view.*
 import zero.network.petgarden.R
 import zero.network.petgarden.model.entity.User
 import zero.network.petgarden.ui.register.OnNextListener
+import zero.network.petgarden.util.onClick
+import zero.network.petgarden.util.show
 import java.util.*
 
 /**
@@ -24,11 +27,33 @@ class BirthRegisterFragment(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View = inflater.inflate(R.layout.fragment_register_birth, container, false).apply {
-        val calendar = Calendar.getInstance();
+        val calendar = Calendar.getInstance()
         calendar.time = user.birthDay;
 
-        birthInputDate.updateDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH))
+        birthInputDate.apply {
+            updateDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH))
+
+            nextButton.onClick {
+                if (yearsSince < 16){
+                    show(getString(R.string.age_error))
+                }else {
+                    user.birthDay = date
+                    listener.next(this@BirthRegisterFragment)
+                }
+            }
+        }
 
     }
+
+    private val DatePicker.yearsSince: Int
+        get() = Calendar.getInstance().get(Calendar.YEAR) - year
+
+    // [DatePicker] to date
+    val DatePicker.date: Date
+        get() {
+            val calendar = Calendar.getInstance()
+            calendar[year, month] = dayOfMonth
+            return calendar.time
+        }
 
 }
--- app/src/main/java/zero/network/petgarden/util/extensions.kt
+++ app/src/main/java/zero/network/petgarden/util/extensions.kt
@@ -1,11 +1,20 @@
 package zero.network.petgarden.util
 
+import android.view.View
+import android.widget.DatePicker
 import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import java.util.*
+
 
 // Get the string text from an EditText
 fun EditText.toText() = this.text.toString()
 
 // Show a toast whit long duration in a fragment
-fun Fragment.show(message: String) = Toast.makeText(this.activity,message,Toast.LENGTH_LONG).show()
+fun Fragment.show(message: String) =
+    Toast.makeText(this.activity, message, Toast.LENGTH_LONG).show()
+
+// Short the setOnCLickListenerMethod
+fun View.onClick(action: (View) -> Unit) = this.setOnClickListener { action(it) }
+
