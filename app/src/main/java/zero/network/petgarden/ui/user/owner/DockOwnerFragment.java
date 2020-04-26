276bacef96ef1414b3265a06befa52216e63a271
rockellar <santiagochasqui11@gmail.com>
2020-04-26 01:30:52 -0500

    Avance dock flotante
    Signed-off-by: rockcellar <santiagochasqui11@gmail.com>

--- app/src/main/java/zero/network/petgarden/ui/user/owner/DockOwnerFragment.java
+++ app/src/main/java/zero/network/petgarden/ui/user/owner/DockOwnerFragment.java
@@ -1,4 +1,4 @@
-package zero.network.petgarden;
+package zero.network.petgarden.ui.user.owner;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import zero.network.petgarden.R;
 
 
 /**
