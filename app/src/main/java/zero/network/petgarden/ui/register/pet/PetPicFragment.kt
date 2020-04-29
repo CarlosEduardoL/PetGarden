285c87eb069846ee190dc02bfc6ef99a37fdfb6f
CarlosEduardoL <carlos.lizalda1@hotmail.com>
2020-04-29 11:26:06 -0500

    Permisos para usar la camara añadidos

--- app/src/main/java/zero/network/petgarden/ui/register/pet/PetPicFragment.kt
+++ app/src/main/java/zero/network/petgarden/ui/register/pet/PetPicFragment.kt
@@ -1,13 +1,15 @@
 package zero.network.petgarden.ui.register.pet
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import zero.network.petgarden.R
 import zero.network.petgarden.databinding.FragmentPetPicBinding
 import zero.network.petgarden.model.entity.Pet
+import zero.network.petgarden.util.onClick
 
 class PetPicFragment(private val listener: OnNextListener, private val pet: Pet) : Fragment() {
 
@@ -15,6 +17,16 @@ class PetPicFragment(private val listener: OnNextListener, private val pet: Pet)
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View = FragmentPetPicBinding.inflate(inflater, container, false).apply {
-
+        cameraButton.onClick {
+            ActivityCompat.requestPermissions(activity!!, arrayOf(
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ), REQUEST_PERMISSION_CODE)
+        }
     }.root
+
+    companion object {
+        private const val REQUEST_PERMISSION_CODE = 0
+    }
 }
