49d1fb11a887c5270fd7eaebf62b25c5316f5e9b
CarlosEduardoL <carlos.lizalda1@hotmail.com>
2020-05-07 15:20:28 -0500

    Cambios en sitter Completos

--- app/src/main/java/zero/network/petgarden/model/behaivor/ISitter.kt
+++ app/src/main/java/zero/network/petgarden/model/behaivor/ISitter.kt
@@ -1,7 +1,26 @@
 package zero.network.petgarden.model.behaivor
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
+import zero.network.petgarden.model.entity.Owner
+import zero.network.petgarden.model.entity.Pet
+
 interface ISitter {
 
+    suspend fun clients(): Set<Owner>
 
+    suspend fun pets(): Set<Pet>
+
+    suspend fun clientsXPets(): Map<Owner, Set<Pet>>
+
+
+    // Java Versions
+
+    fun clients(callBack: CallBack<Set<Owner>>) = CoroutineScope(Main).launch { callBack.onResult(clients()) }
+
+    fun pets(callBack: CallBack<Set<Pet>>) = CoroutineScope(Main).launch { callBack.onResult(pets()) }
+
+    fun clientsXPets(callBack: CallBack<Map<Owner,Set<Pet>>>) = CoroutineScope(Main).launch { callBack.onResult(clientsXPets()) }
 
 }
--- app/src/main/java/zero/network/petgarden/model/entity/Sitter.kt
+++ app/src/main/java/zero/network/petgarden/model/entity/Sitter.kt
@@ -8,6 +8,7 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import zero.network.petgarden.model.behaivor.Entity
 import zero.network.petgarden.model.behaivor.IPlanner
+import zero.network.petgarden.model.behaivor.ISitter
 import zero.network.petgarden.model.behaivor.IUser
 import zero.network.petgarden.tools.downloadImage
 import zero.network.petgarden.util.saveURLImageOnFile
@@ -23,10 +24,63 @@ data class Sitter(
     var additional: String = "Nothing Especial",
     val clients: MutableList<String> = mutableListOf(),
     private val planner: Planner = Planner()
-) : IUser by user, IPlanner by planner, Serializable, Entity {
+) : IUser by user, IPlanner by planner, Serializable, Entity, ISitter {
 
     val availability: Duration?
         get() = planner.availabilities.min()
+
+    private var _clients: MutableSet<Owner>? = null
+    private var _pets: MutableSet<Pet>? = null
+
+    override suspend fun clients(): Set<Owner> {
+        _clients?.let {
+            return it
+        }
+        downloadPets()
+        return clients()
+    }
+
+    override suspend fun pets(): Set<Pet> {
+        _pets?.let {
+            return it
+        }
+        downloadPets()
+        return pets()
+    }
+
+    override suspend fun clientsXPets(): Map<Owner, Set<Pet>> {
+        return clients().map {
+            it to pets().filter { pet -> pet.ownerID == it.id }.toSet()
+        }.toMap()
+    }
+
+    private suspend fun downloadPets(){
+        val query = FirebaseDatabase.getInstance().reference
+            .child(Pet.FOLDER).orderByChild("sitterID").equalTo(id)
+        _pets = suspendCoroutine { continuation ->
+            query.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(e: DatabaseError) =
+                    continuation.resumeWithException(e.toException())
+                override fun onDataChange(data: DataSnapshot) = continuation.resume(mutableSetOf<Pet>().apply{
+                    addAll(data.children.map { it.getValue(Pet::class.java)!! })
+                })
+            })
+        }!!
+    }
+
+    private suspend fun downloadOwners(){
+        val query = FirebaseDatabase.getInstance().reference
+            .child(Owner.FOLDER).orderByChild("sitterList").equalTo(id)
+        _clients = suspendCoroutine { continuation ->
+            query.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(e: DatabaseError) =
+                    continuation.resumeWithException(e.toException())
+                override fun onDataChange(data: DataSnapshot) = continuation.resume(mutableSetOf<Owner>().apply{
+                    addAll(data.children.map { it.getValue(Owner::class.java)!! })
+                })
+            })
+        }!!
+    }
 
     suspend fun image(): Bitmap {
         imageURL?.let {
