d11e527d1c22be0d9213ec20143b5dc9253499cb
rockellar <santiagochasqui11@gmail.com>
2020-05-10 13:33:08 -0500

    Error al iniciar como sitter arreglado

--- app/src/main/java/zero/network/petgarden/ui/user/sitter/SitterActivity.java
+++ app/src/main/java/zero/network/petgarden/ui/user/sitter/SitterActivity.java
@@ -16,28 +16,47 @@ import com.google.firebase.database.FirebaseDatabase;
 import zero.network.petgarden.R;
 import zero.network.petgarden.model.entity.Owner;
 import zero.network.petgarden.model.entity.Sitter;
+import zero.network.petgarden.ui.element.ActionBarFragment;
+import zero.network.petgarden.ui.user.owner.DockFragment;
 import zero.network.petgarden.ui.user.owner.MapFragment;
 
 public class SitterActivity extends AppCompatActivity {
 
     private Sitter sitter;
-    MapFragment fragmentMap;
+    MapSitterFragment fragmentMap;
     FragmentManager fragmentManager;
+    private ActionBarFragment topBarFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sitter);
 
+        loadInitialFragments();
+
         loadDataFromActivity();
 
+    }
+
+    private void loadInitialFragments() {
+
+        topBarFragment = new ActionBarFragment("",false);
+        FragmentTransaction transaction1 = getSupportFragmentManager().beginTransaction();
+        transaction1.replace(R.id.topBarSitter,topBarFragment, null);
+        transaction1.commit();
+
+        DockSitterFragment dockFragment = new DockSitterFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.dock_container_sitter,dockFragment, null);
+        transaction.commit();
     }
 
     private void loadDataFromActivity() {
 
         //PONER A CARGAR LOS DATOS DEL EXTRA
-/*        Bundle extras = getIntent().getExtras();
-        owner =(Owner) extras.getSerializable("user");*/
+        Bundle extras = getIntent().getExtras();
+        sitter =(Sitter) extras.getSerializable("user");
 
 
     }
@@ -51,11 +70,11 @@ public class SitterActivity extends AppCompatActivity {
 
 
         if( ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            fragmentMap = new MapFragment();
+            fragmentMap = new MapSitterFragment();
 
             fragmentManager =getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.add(R.id.activity_owner_container,fragmentMap, null);
+            fragmentTransaction.replace(R.id.actualFragmentContainerSitter,fragmentMap, null);
             fragmentTransaction.commit();
         }
 
